03f8caeb89d4fb013c11d109f729f6c8acbc5b2b
Landry Barb II <landrybarb@pm.me>
2025-12-18 07:31:40 -0800

    refactor(App.vue) added semantic markup.  installed vue router and sass

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { RouterView } from 'vue-router';
+  import SkipLink from './components/atoms/SkipLink.vue';
+  import MainNav from './components/MainNav.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <SkipLink/>
+
+  <header role="banner" class="app-header">
+    <MainNav/>
+  </header>
+
+  <main id="main-content" role="main" tabindex="-1">
+    <RouterView />
+  </main>
+
+  <footer role="contentinfo" class="app-footer">
+    <p>&copy; 2025 NeonStream. Compliance: WCAG 2.2 AAA.</p>
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss" scoped>
+    :focus-visible{
+    outline: 3px solid #ffcc00;
+    outline-offset: 2px;
+  }
+#main{
+   outline: none;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
